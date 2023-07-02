51b79d823de9ae8f4dd2a176d7e430276fd4a324
Naratip <naratip.18257@gmail.com>
2023-07-02 13:54:15 +0700

    add function create from

--- src/index.js
+++ src/index.js
@@ -62,3 +62,68 @@ function createListItem(title,descrip,dueDate,priority){
 
     return item;
 }
+
+function update(){
+    const content = document.querySelector(".content");
+    const body = document.querySelector(".content");
+
+    list.sort((a,b)=>b.priority - a.priority);
+
+    const newContent = document.createElement("div");
+    newContent.classList.add("content");
+
+    list.forEach(e => {newContent.appendChild(e.div)});
+
+    body.removeChild(content);
+    body.appendChild(newContent);
+}
+
+function createFrom(){
+    const div = document.createElement("div");
+    div.classList.add("from");
+
+    const title = document.createElement("div");
+    title.classList.add("from-item");
+    const label1 = document.createElement("label");
+    label1.setAttribute("for","title");
+    label1.textContent = "Title:";
+    const input1 = document.createElement("input");
+    input1.setAttribute("id","title");
+    title.appendChild(label1);
+    title.appendChild(input1);
+
+    const des = document.createElement("div");
+    des.classList.add("from-item");
+    const label2 = document.createElement("label");
+    label2.setAttribute("for","des");
+    label2.textContent = "Description:";
+    const input2 = document.createElement("textarea");
+    input2.setAttribute("id","des");
+    des.appendChild(label2);
+    des.appendChild(input2);
+
+    const priority = document.createElement("div");
+    priority.classList.add("from-item");
+    const label3 = document.createElement("label");
+    label3.setAttribute("for","priority");
+    label3.textContent = "Priority:";
+    const input3 = document.createElement("input");
+    input3.setAttribute("id","priority");
+    input3.setAttribute("type","number");
+    input3.setAttribute("min","0");
+    input3.setAttribute("max","10");
+    priority.appendChild(label3);
+    priority.appendChild(input3);
+
+    const date = document.createElement("div");
+    date.classList.add("from-item");
+    const label4 = document.createElement("label");
+    label4.setAttribute("for","date");
+    label4.textContent = "Due Date:";
+    const input4 = document.createElement("input");
+    input4.setAttribute("id","date");
+    input4.setAttribute("type","date");
+    date.appendChild(label4);
+    date.appendChild(input4);
+    
+}
