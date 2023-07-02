4a0fdb305532d951a9b3329058ad75b87db63dfb
Naratip <naratip.18257@gmail.com>
2023-07-02 13:17:26 +0700

    add function create list item

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function pageLoad(){
 
 let list = [];
 
-const ToDoList = (title,descrip,dueDate,priority)=>{
+const ToDoList = (title,descrip,dueDate,priority,div)=>{
     const proto = {
         add(){
             list.push(this);
@@ -24,5 +24,41 @@ const ToDoList = (title,descrip,dueDate,priority)=>{
             list = list.filter( e => e !== this);
         },
     }
-    return Object.assign(Object.create(proto),{title,descrip,dueDate,priority});
+    return Object.assign(Object.create(proto),{title,descrip,dueDate,priority,div});
+}
+
+function createListItem(title,descrip,dueDate,priority){
+    const div = document.createElement("div");
+    div.classList.add("item");
+
+    const item = ToDoList(title,descrip,dueDate,priority,div);
+    
+    const head = document.createElement("h2");
+    head.classList.add("title");
+    head.textContent = item.title;
+    div.appendChild(head);
+
+    const btn = document.createElement("button");
+    btn.setAttribute("type","button");
+    btn.textContent = "X";
+    div.appendChild(btn);
+
+    const date = document.createElement("div");
+    date.classList.add("date");
+    date.textContent = item.dueDate;
+    div.appendChild(date)
+
+    const des = document.createElement("div");
+    des.classList.add("des");
+    des.textContent = item.descrip;
+    div.appendChild(des);
+
+
+    btn.addEventListener("click",(e)=>{
+        const content = document.querySelector(".content");
+        item.remove();
+        content.removeChild(div);
+    })
+
+    return item;
 }
